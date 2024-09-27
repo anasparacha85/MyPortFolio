9da386bf17850b00db3afa50d95fd6d95e9e3557
anasparacha85 <anasparacha85@gmail.com>
2024-09-27 03:25:04 -0700

    portfolio made

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+
+    </>
+  )
+}
+
+export default App
